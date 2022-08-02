9d4ac0093513131c14d21bf2eb20a4986b2b10f3
ram <ram@gmail.com>
2022-08-02 15:37:10 +0530

    update wrorking

--- lib/models/wallet_model.dart
+++ lib/models/wallet_model.dart
@@ -1,8 +1,9 @@
 class WalletBalance{
   String code,symbol,image;
   double amount;
+  int isActive;
 
-  WalletBalance(this.code, this.amount,this.symbol,this.image);
+  WalletBalance(this.code, this.amount,this.symbol,this.image,this.isActive);
 
   factory WalletBalance.fromJson(dynamic json) {
     return WalletBalance(
@@ -10,6 +11,7 @@ class WalletBalance{
       double.tryParse(json['amount'].toString()) ?? 0,
       json['symbol'] ?? '',
       json['image'] ?? '',
+      json['is_active'] ?? 0,
     );
   }
 }
