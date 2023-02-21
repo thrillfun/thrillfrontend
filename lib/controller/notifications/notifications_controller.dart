1d0d88a09c40f5a64d3f16c6d4bb82eae5d33a2b
anantmann <anantmann9057@gmail.com>
2023-02-21 13:19:21 +0530

    better video player improvements

---
+++ lib/controller/notifications/notifications_controller.dart
@@ -0,0 +1,87 @@
+import 'dart:convert';
+
+import 'package:dio/dio.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:logger/logger.dart';
+import 'package:thrill/utils/util.dart';
+
+import '../../rest/rest_url.dart';
+
+class NotificationsController extends GetxController {
+  var dio = Dio(BaseOptions(baseUrl: RestUrl.baseUrl));
+
+  Future<void> sendChatNotifcations(int id, String message,
+      {String? username}) async {
+    dio.options.headers = {
+      "Authorization": "Bearer ${await GetStorage().read("token")}"
+    };
+    dio.post("/user/send-chat-notification", queryParameters: {
+      "other_user_id": "$id",
+      "message": message,
+    }).then((value) {
+      if (value.data["status"] == false) {
+        errorToast(value.data["message"]);
+      } else {
+        successToast(value.data["message"]);
+      }
+      Logger().wtf(value.data["data"]);
+    }).onError((error, stackTrace) {
+      errorToast(error.toString());
+    });
+  }
+
+  Future<void> sendFcmNotification(String fcmToken,
+      {String? body = "", String? title = "", String? image = ""}) async {
+    var dio = Dio(BaseOptions(baseUrl: "https://fcm.googleapis.com/fcm"));
+    dio.options.headers = {
+      "Authorization":
+          "key= AAAAzWymZ2o:APA91bE0GtToqnK6DaV9MVEOMzBdPSfYP7gSqUlcNeV4Iwmg_p3byDHq2m5Dfw8OgC9sZzK4cksjSGiO-pH-dNqgnM8loBt7eTtFO3jI-nD1KKDItuO2g4LIqPHpdFX6mPULUwfIipu7"
+    };
+    final data = {
+      "to": fcmToken,
+      "notification": {"body": body, "title": title, "image": image},
+      "priority": "high",
+      "image": image,
+      "data": {
+        "click_action": "FLUTTER_NOTIFICATION_CLICK",
+        "id": "1",
+        "status": "done",
+        "image":
+            "https://scontent.fbom19-2.fna.fbcdn.net/v/t39.30808-6/271720827_4979339162088555_3028905257532289818_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=HMgk-tDtBcQAX9uJheY&_nc_ht=scontent.fbom19-2.fna&oh=00_AfCVE7nSsxVGPTfTa8FCyff4jOzTKWi_JvTXpDWm7WrVjg&oe=63E84FB2"
+      }
+    };
+    dio.post("/send", data: jsonEncode(data)).then((value) {
+      Logger().wtf(value);
+    }).onError((error, stackTrace) {
+      Logger().wtf(error);
+    });
+  }
+
+  Future<void> sendMultipleFcmNotifications(List<String> fcmToken,
+      {String? body = "", String? title = "", String? image = ""}) async {
+    var dio = Dio(BaseOptions(baseUrl: "https://fcm.googleapis.com/fcm"));
+    dio.options.headers = {
+      "Authorization":
+          "key=AAAAzWymZ2o:APA91bGABMolgt7oiBiFeTU7aCEj_hL-HSLlwiCxNGaxkRl385anrsMMNLjuuqmYnV7atq8vZ5LCNBPt3lPNA1-0ZDKuCJHezvoRBpL9VGvixJ-HHqPScZlwhjeQJPhbsiLDSTtZK-MN"
+    };
+    final data = {
+      "registration_ids": fcmToken,
+      "notification": {"title": title, "body": body, "image": image},
+      "priority": "high",
+      "image": image,
+      "data": {
+        "click_action": "FLUTTER_NOTIFICATION_CLICK",
+        "id": "1",
+        "status": "done",
+        "image":
+            "https://scontent.fbom19-2.fna.fbcdn.net/v/t39.30808-6/271720827_4979339162088555_3028905257532289818_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=HMgk-tDtBcQAX9uJheY&_nc_ht=scontent.fbom19-2.fna&oh=00_AfCVE7nSsxVGPTfTa8FCyff4jOzTKWi_JvTXpDWm7WrVjg&oe=63E84FB2"
+      }
+    };
+    dio.post("/send", data: jsonEncode(data)).then((value) {
+      Logger().wtf(value);
+    }).onError((error, stackTrace) {
+      Logger().wtf(error);
+    });
+  }
+}
